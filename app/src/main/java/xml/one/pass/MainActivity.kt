7d7b9dc4b932b51f66a92a3b7cf6a6f0dcec7cfd
Kelvin Kioko <kelvinmkioko@gmail.com>
2022-07-10 12:37:01 +0300

    [UI] Implemented all the required UIs

--- app/src/main/java/xml/one/pass/MainActivity.kt
+++ app/src/main/java/xml/one/pass/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_OnePass)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
