d03a41b7eb294b2fd692d8cf892117f2ba51bb8d
Kelvin Kioko <kelvinmkioko@gmail.com>
2022-07-11 18:29:15 +0300

    [Forgot Password] Implemented the forgot password logic and navigation

--- app/src/main/java/xml/one/pass/presentation/forgot_password/ForgotPasswordFragment.kt
+++ app/src/main/java/xml/one/pass/presentation/forgot_password/ForgotPasswordFragment.kt
@@ -2,21 +2,32 @@ package xml.one.pass.presentation.forgot_password
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import xml.one.pass.R
 import xml.one.pass.databinding.ForgotPasswordFragmentBinding
 import xml.one.pass.extension.viewBinding
+import xml.one.pass.util.ConstantsUtil
 
+@AndroidEntryPoint
 class ForgotPasswordFragment : Fragment(R.layout.forgot_password_fragment) {
 
     private val binding by viewBinding(ForgotPasswordFragmentBinding::bind)
+    private val viewModel: ForgotPasswordViewModel by viewModels()
+
+    private var emailAddress = ""
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setupToolbar()
         setUpOnClickListener()
+        setUpObserver()
     }
 
     private fun setupToolbar() {
@@ -28,10 +39,58 @@ class ForgotPasswordFragment : Fragment(R.layout.forgot_password_fragment) {
     private fun setUpOnClickListener() {
         binding.apply {
             submitAction.setOnClickListener {
-                findNavController().navigate(
-                    ForgotPasswordFragmentDirections.toResetPasswordFragment()
-                )
+                if (ConstantsUtil.EMAIL_REGEX.toRegex().matches(emailAddress)) {
+                    viewModel.checkAccountExists(email = emailAddress)
+                } else {
+                    emailAddressInput.error = "Please enter a valid email address"
+                }
             }
         }
+    }
+
+    private fun setUpObserver() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.forgotPasswordUiState.collect { state ->
+                when (state) {
+                    is ForgotPasswordUiState.Error ->
+                        Snackbar.make(binding.root, state.message, Snackbar.LENGTH_LONG).show()
+                    is ForgotPasswordUiState.Loading ->
+                        Snackbar.make(
+                            binding.root,
+                            if (state.isLoading) "Loading" else "Not Loading",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    ForgotPasswordUiState.Success -> findNavController().navigate(
+                        ForgotPasswordFragmentDirections.toResetPasswordFragment()
+                    )
+                }
+            }
+        }
+    }
+
+    private fun setUpInputListener() {
+        binding.apply {
+            emailAddressInput.editText?.doAfterTextChanged {
+                emailAddress = if (it.isNullOrEmpty()) "" else it.toString()
+                emailAddressInput.error = null
+                submitAction.isEnabled = emailAddress.isNotEmpty()
+            }
+        }
+    }
+
+    private fun removeInputListener() {
+        binding.apply {
+            emailAddressInput.editText?.doAfterTextChanged {}
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setUpInputListener()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        removeInputListener()
     }
 }
---
+++ app/src/main/java/xml/one/pass/presentation/forgot_password/ForgotPasswordViewModel.kt
@@ -0,0 +1,51 @@
+package xml.one.pass.presentation.forgot_password
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import xml.one.pass.domain.repository.AccountRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class ForgotPasswordViewModel @Inject constructor(
+    private val accountRepository: AccountRepository
+) : ViewModel() {
+
+    private val _forgotPasswordUiState = MutableStateFlow<ForgotPasswordUiState>(
+        ForgotPasswordUiState.Loading(isLoading = false)
+    )
+    val forgotPasswordUiState: StateFlow<ForgotPasswordUiState> = _forgotPasswordUiState
+
+    fun checkAccountExists(email: String) {
+        viewModelScope.launch {
+            _forgotPasswordUiState.value = ForgotPasswordUiState.Loading(isLoading = true)
+            withContext(Dispatchers.IO) {
+                delay(2000)
+                val accountExists = accountRepository.doesAccountExistWithEmail(email = email)
+                withContext(Dispatchers.Main) {
+                    _forgotPasswordUiState.value = ForgotPasswordUiState.Loading(isLoading = false)
+                    _forgotPasswordUiState.value = if (accountExists)
+                        ForgotPasswordUiState.Success
+                    else
+                        ForgotPasswordUiState.Error(
+                            message = "No account exists with under that E-mail address"
+                        )
+                }
+            }
+        }
+    }
+}
+
+sealed class ForgotPasswordUiState {
+    data class Loading(val isLoading: Boolean) : ForgotPasswordUiState()
+
+    data class Error(val message: String) : ForgotPasswordUiState()
+
+    object Success : ForgotPasswordUiState()
+}
